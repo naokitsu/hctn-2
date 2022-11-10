e2571fb1bee05556cc41c5ac10ddd3320054f10e
Naokitsu <naokitsu@tutanota.com>
2022-11-11 01:33:23 +0300

    Initial commit

---
+++ html.py
@@ -0,0 +1,83 @@
+html_template = """
+<head>
+    <style>
+    .tab {{
+        overflow: hidden;
+        border: 1px solid #ccc;
+        background-color: #f1f1f1;}}
+    .tab button {{
+            background-color: inherit;
+            float: left;
+            border: none;
+            outline: none;
+            cursor: pointer;
+            padding: 8px 8px;
+            transition: 0.3s;
+        }}
+        .tab button:hover {{
+            background-color: #ddd;
+        }}
+        .tab button.active {{
+            background-color: #ccc;
+        }}
+        .tabcontent {{
+            display: none;
+            border: 1px solid #ccc;
+        }}
+    </style>
+    <meta charset="UTF-8">
+<body style="padding: 10px">
+<div class="tab">
+    <button class="groupLinks" id="defaultOpen" onclick="openGroup(event, 'Positive')">Файлы влияющие на цвет заголовка</button>
+    <button class="groupLinks" onclick="openGroup(event, 'Negative')">Файлы не влияющие на цвет заголовка</button>
+</div>
+<div id="Positive" class="group">
+    <div class="tab">
+        {}
+    </div>
+</div>
+<div id="Negative" class="group">
+    <div class="tab">
+        {}
+    </div>
+</div>
+{}
+<script>
+    function openGroup(evt, groupName) {
+        var i, content, buttons;
+        content = document.getElementsByClassName("group");
+        for (i = 0; i < content.length; i++) {
+            content[i].style.display = "none";
+        }
+        buttons = document.getElementsByClassName("groupLinks");
+        for (i = 0; i < buttons.length; i++) {
+            buttons[i].className = buttons[i].className.replace(" active", "");
+        }
+        document.getElementById(groupName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+    function openEntry(evt, entryName) {
+        var i, content, buttons;
+        content = document.getElementsByClassName("entry");
+        for (i = 0; i < content.length; i++) {
+            content[i].style.display = "none";
+        }
+        buttons = document.getElementsByClassName("entryLinks");
+        for (i = 0; i < buttons.length; i++) {
+            buttons[i].className = buttons[i].className.replace(" active", "");
+        }
+        document.getElementById(entryName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+</script>
+<script>
+    document.getElementById("defaultOpen").click();
+</script>
+</body>
+</html>
+"""
+
+header_template = """
+<h2>Заголовок {}</h2>
+<p>Тег "color": <tagname style="color:green">совпадает</tagname> (Эталон: rgb(0, 0, 0))</p><p>Сравнение скриншотов: <tagname style="color:green">совпадают</tagname></p><div style="border: 1px solid #ccc"><img src="./images/7fc365ec-e5ec-4385-84c8-835f29c12993/489152de-4b61-4919-99bd-07d33e01580e.png"><br><img src="./images/without-styles/489152de-4b61-4919-99bd-07d33e01580e.png"></div>
+"""
---
+++ main.py
@@ -0,0 +1,123 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+import os
+from PIL import Image
+from PIL import ImageChops
+import shutil
+import html
+
+def check_we(we: WebElement):
+    """
+    Проверка WebElement на файл стилей headers.css
+    :return: True, если we не является блоком подключающим headers.css
+    """
+    return (we.get_attribute("rel") == "stylesheet") & (not ("headers.cc" in we.get_attribute("href")))
+
+
+def compare_images(path_one: str, path_two: str):
+    """
+    Сравнение двух изображений
+    :return: True, если индентичны; False, если изображения отличаются
+    """
+    image_one = Image.open(path_one).convert("RGB")
+    image_two = Image.open(path_two).convert("RGB")
+    diff = ImageChops.difference(image_one, image_two)
+
+    return not(diff.getbbox())
+
+
+def main():
+    """
+    Точка входа
+    """
+    driver = webdriver.Firefox()
+    driver.get("http://localhost:63343/page/index.html?_ijt=qv7qe56021c58sku5nb7mdfdrk&_ij_reload=RELOAD_ON_SAVE")
+
+    shutil.rmtree("./images")
+    os.mkdir("./images")
+    os.mkdir("./images/without-styles")
+
+    # --- Configs
+    # Список стилей, которые необходимо протестировать, функция check_we фильтрует итоговый список
+    stylesheets_to_test = [i for i in driver.find_elements(by="tag name", value="link") if check_we(i)]
+    # Элементы которые необходиимо протестировать
+    elements_to_test = driver.find_elements(by="tag name", value="h2")
+    # Тестируемое свойство
+    property_to_test = "color"
+    # ---
+
+    stylesheet_colors = {}
+    stylesheet_id_to_path = {}
+
+    # Выключаем все тестируемые стили
+    for stylesheet in stylesheets_to_test:
+        driver.execute_script("arguments[0].disabled = true", stylesheet)
+
+    # Делаем скриншоты и запоминаем оригинальные цвета заголовков
+    colors = {}
+    for element in elements_to_test:
+        element.screenshot(f"./images/without-styles/{element.id}.png")
+        colors[element.id] = element.value_of_css_property(property_to_test)
+    stylesheet_colors["original"] = colors
+
+    # Включаем по одному, делаем скриншоты, запоминаем цвета в мапу
+    for stylesheet in stylesheets_to_test:
+        driver.execute_script("arguments[0].disabled = false", stylesheet)
+        colors = {}
+        os.mkdir(f"./images/{stylesheet.id}")
+        for element in elements_to_test:
+            element.screenshot(f"./images/{stylesheet.id}/{element.id}.png")
+            colors[element.id] = element.value_of_css_property(property_to_test)
+        stylesheet_colors[stylesheet.id] = colors
+        stylesheet_id_to_path[stylesheet.id] = stylesheet.get_attribute("href")
+        driver.execute_script("arguments[0].disabled = true", stylesheet)
+
+    # Сравниваем
+    positive_stylesheets = []
+    negative_stylesheets = []
+    message = ""
+    for stylesheet_name in stylesheet_colors:
+        if stylesheet_name == 'original':
+            continue
+        positive = False
+        message += f'<div id="{stylesheet_name}" class="entry tabcontent">'
+        message += f"<h1>Файл стилей: {stylesheet_id_to_path[stylesheet_name]}</h1>"
+        for header_name in stylesheet_colors[stylesheet_name]:
+            message += f'<h2>Заголовок {header_name}</h2>'
+            message += f'\n<p>Тег "color": '
+            positive = stylesheet_colors[stylesheet_name][header_name] != stylesheet_colors["original"][header_name]
+            if stylesheet_colors[stylesheet_name][header_name] == stylesheet_colors["original"][header_name]:
+                message += f'<tagname style="color:green">совпадает</tagname> ' \
+                           f'(Эталон: {stylesheet_colors["original"][header_name]}))'
+            else:
+                message += f'<tagname style="color:red">несовпадает</tagname> ' \
+                           f'(Эталон: {stylesheet_colors["original"][header_name]}, текущий: {stylesheet_colors[stylesheet_name][header_name]})'
+
+
+            message += f'</p><p>Сравнение скриншотов: '
+            if compare_images(f"./images/{stylesheet_name}/{header_name}.png", f"./images/without-styles/{header_name}.png"):
+                message += '<tagname style="color:green">совпадают</tagname>'
+            else:
+                message += '<tagname style="color:green">совпадают</tagname>'
+            message += f'</p><div style="border: 1px solid #ccc"><img src="./images/{stylesheet_name}/{header_name}.png"><br><img src="./images/without-styles/{header_name}.png"></div>'
+            message += '\n'
+        message += "</div>"
+        if positive:
+            positive_stylesheets.append(stylesheet_name)
+        else:
+            negative_stylesheets.append(stylesheet_name)
+
+    with open('report.html', 'w') as f:
+        positive_buttons = ""
+        negative_buttons = ""
+        for i in positive_stylesheets:
+            positive_buttons += f'<button class="groupLinks" onclick="openEntry(event, \'{i}\')">{stylesheet_id_to_path[i]}</button>\n'
+        for i in negative_stylesheets:
+            negative_buttons += f'<button class="groupLinks" onclick="openEntry(event, \'{i}\')">{stylesheet_id_to_path[i]}</button>\n'
+        f.write(html.html_template.format(positive_buttons, negative_buttons, message))
+    driver.quit()
+
+
+if __name__ == '__main__':
+    main()
